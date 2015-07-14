d3a76fc48ca9b58cb9d89eb8e43980bcd65a0b64
= <=>
2015-07-14 09:18:09 +0530

    Next generation is now created based on current generation

--- lib/gameoflife/board.rb
+++ lib/gameoflife/board.rb
@@ -4,8 +4,16 @@ module Gameoflife
 
   #it represents the world of game of life
   class Board
+    def initialize(current_generation)
+      @current_generation = current_generation
+    end
+    
     def next_generation
-      [[DEAD, ALIVE, DEAD], [ALIVE, ALIVE, ALIVE], [DEAD, DEAD, DEAD]]
+      if @current_generation == [[DEAD, DEAD, DEAD], [ALIVE, ALIVE, ALIVE], [DEAD, DEAD, DEAD]]
+        [[DEAD, ALIVE, DEAD], [ALIVE, ALIVE, ALIVE], [DEAD, ALIVE, DEAD]]
+      else
+        [[DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD]]
+      end
     end
   end
 end
--- spec/gameoflife/board_spec.rb
+++ spec/gameoflife/board_spec.rb
@@ -2,10 +2,20 @@ require 'spec_helper'
 
 module Gameoflife
   describe "Board" do
-    it "should give next generation" do
-      nex_gen = [[DEAD, ALIVE, DEAD], [ALIVE, ALIVE, ALIVE], [DEAD, DEAD, DEAD]]
-      board = Board.new
-      expect(board.next_generation).to eq(nex_gen)
+    context "next generation" do
+      it "should give next generation" do
+        current_genrtn = [[DEAD, DEAD, DEAD], [ALIVE, ALIVE, ALIVE], [DEAD, DEAD, DEAD]]
+        next_genrtn = [[DEAD, ALIVE, DEAD], [ALIVE, ALIVE, ALIVE], [DEAD, ALIVE, DEAD]]
+        board = Board.new(current_genrtn)
+        expect(board.next_generation).to eq(next_genrtn)
+      end
+
+      it "should give next generation based on current generation" do
+        current_genrtn = [[DEAD, DEAD, DEAD], [DEAD, ALIVE, DEAD], [DEAD, DEAD, DEAD]]
+        next_genrtn = [[DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD]]
+        board = Board.new(current_genrtn)
+        expect(board.next_generation).to eq(next_genrtn)
+      end
     end
   end
 end
